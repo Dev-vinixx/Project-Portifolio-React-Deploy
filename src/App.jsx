c63a14d59c782bf10d8171127000c909e771a981
Dev-vinixx <viniciuseduardo0500@gmail.com>
2023-10-12 21:20:37 -0300

    first stage of the portfolio complete now I will do the projects area.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import './App.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
+
 
 
 
 function App() {
 
   return (
-    <>
+    <body>
 
-      
+
       <Header />
-     
-    </>
+
+        <section className="section">
+
+          <div className="div-text">
+            <p className="text-one">hello 👋🏻, my name is Vinicius i am</p>
+            <span>react</span>
+            <p className="text-two">Developer</p>
+            <p className="text-three">I've been a programmer <br /> for 7 months but I'm currently a freelancer.</p>
+          </div>
+
+          <img className="undraw-img" src="./code-developer.svg" alt="" />
+
+        </section>
+
+      <Footer />
+
+    </body>
   )
 }
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,14 +6,14 @@ function Header() {
 
     <header className="header">
 
-    <nav className="nav">
+      <nav className="nav">
         <span><a href="">Dev-vinixx.gitHub</a></span>
         <div className="icons">
           <div className="div-icon">
-            <a href="https://github.com/Dev-vinixx"><img className="img-icon"  src="/github.svg" alt="svg git hub" /></a>
+            <a href="https://github.com/Dev-vinixx"><img className="img-icon" src="/github.svg" alt="svg git hub" /></a>
           </div>
           <div className="div-icon">
-            <a href="www.linkedin.com/in/vinícius-rodrigues-17a825280"><img className="img-icon"  src="/linkedin.svg" alt="svg linkedin" /></a>
+            <a href="www.linkedin.com/in/vinícius-rodrigues-17a825280"><img className="img-icon" src="/linkedin.svg" alt="svg linkedin" /></a>
           </div>
           <div className="div-icon">
             <a href=""><img className="img-icon" src="/clipboard.svg" alt="svg curriculo" /></a>
